86c03ff40c00e9ae166e718feee55de6a0ec4e79
rafaelepsouza <rafaelexpedito.011@gmail.com>
2024-04-16 14:13:03 -0300

    projeto-dinho-r.s

---
+++ script.js
@@ -0,0 +1,14 @@
+function alerta(){
+    alert("Você será redirecionado ao WhatsApp!!")
+}
+
+
+const form = document.querySelector("form")
+const fechar = document.querySelector(".close-img")
+const nav = document.querySelector("header")
+const body = document.querySelector("body")
+
+
+
+
+
